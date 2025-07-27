9c6ff3d556427dac152060685a52398f273773c6
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-27 22:40:37 +1000

    Update voice_to_text.py

--- app/services/voice_to_text.py
+++ app/services/voice_to_text.py
@@ -1,1 +1,19 @@
+import speech_recognition as sr
+import tempfile
+
+def convert_voice_to_text(audio_file):
+    recognizer = sr.Recognizer()
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as tmp:
+        tmp.write(audio_file.read())
+        tmp_path = tmp.name
+
+    with sr.AudioFile(tmp_path) as source:
+        audio = recognizer.record(source)
+        try:
+            text = recognizer.recognize_google(audio)
+            return text
+        except sr.UnknownValueError:
+            return "Could not understand audio"
+        except sr.RequestError as e:
+            return f"Error from Google Speech Recognition service: {e}"
 
