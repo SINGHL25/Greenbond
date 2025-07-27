f21adb9878e393b19d8e6b7b55993008f99ea572
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-27 22:53:06 +1000

    Update plant_model.py

--- backend/ models/plant_model.py
+++ backend/ models/plant_model.py
@@ -1,1 +1,15 @@
+# 📄 backend/models/plant_model.py
 
+plant_schema = {
+    "name": str,
+    "photo_url": str,
+    "location": {
+        "lat": float,
+        "lon": float
+    },
+    "uploaded_by": str,
+    "health_status": str,
+    "growth_timeline": list,
+    "journal_entries": list,
+    "timestamp": str
+}
